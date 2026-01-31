56bafc5e4eb86873bcbee2f3e62c3e1b3125785d
Christopher Vote <votec@oregonstate.edu>
2026-02-01 01:17:49 +0800

    fizz_buzz.c is drafted

--- DS_in_C/functions/fizz_buzz/fizz_buzz.c
+++ DS_in_C/functions/fizz_buzz/fizz_buzz.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+#define FIZZ "fizz"
+#define BUZZ "buzz"
+#define FIZZBUZZ "fizzbuzz"
+
+/* Program name: fizz_buzz.c
+ * Author: Christopher Vote
+ * Description: This program processes an array of integers and prints a modified form. 
+ * 		Values divisible by both 3 and 5 are replaced with "fizz_buzz". Values divisible
+ * 		by 3 are replaced with "fizz", and values divisible by 5 are replaced with "buzz".
+*/
 
 void buildArray(int* arr, int arr_len, int lower, int jump);
+void fizzBuzz(int* arr, int arr_len);
 
 int main() {
 
 	// Example 1
-	printf("\n# fizz_buzz example 1\n");
+	printf("# fizz_buzz example 1\n");
 	int arr1[7];
 	int arr_len1 = 7;
 
@@ -17,6 +28,38 @@ int main() {
 
 }
 
+void fizzBuzz(int* arr, int arr_len) {
+
+	printf("[");
+	for (int i = 0; i < arr_len-1; i++) {
+		if ((arr[i] % 5 == 0) && (arr[i] % 3 == 0)) {
+			printf("%s, ", FIZZBUZZ);
+		}
+		else if (arr[i] % 3 == 0) {
+			printf("%s, ", FIZZ);
+		}
+		else if (arr[i] % 5 == 0) {
+			printf("%s, ", BUZZ);
+		}
+		else {
+			printf("%d, ", arr[i]);
+		}
+	}
+	int endVal = arr[arr_len-1];
+	if ((endVal % 5 == 0) && (endVal % 3 == 0)) {
+		printf("%s]\n", FIZZBUZZ);
+	}
+	else if (endVal % 3 == 0) {
+		printf("%s]\n", FIZZ);
+	}
+	else if (endVal % 5 == 0) {
+		printf("%s]\n", BUZZ);
+	}
+	else {
+		printf("%d]\n", endVal);
+	}
+}
+
 
 void buildArray(int* arr, int arr_len, int lower, int jump) {
 	
@@ -26,13 +69,11 @@ void buildArray(int* arr, int arr_len, int lower, int jump) {
 	}
 	
 	// Print the array
-	printf("Array length: %d\n", arr_len);
+	printf("ARR Size: %d ", arr_len);
 
-	printf("Array: [");
+	printf("[");
 	for (int i = 0; i < arr_len-1; i++) {
 		printf("%d, ", arr[i]);
 	}
-	printf("%d]\n\n", arr[arr_len-1]);
-
-
+	printf("%d]\n", arr[arr_len-1]);
 }
